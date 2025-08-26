f1f49737d0eff8dd03b6afbee78bfe6b898d4fb4
Geetesh <geetesh472@gmail.com>
2025-08-26 19:13:44 +0530

    docker initializing

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    return res.send("<h1>This is backend for docker<h1/>");
+});
+
+app.get("/content", (req, res) => {
+    return res.send("<h1>This is nodemon for docker<h1/>")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
